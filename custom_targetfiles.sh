aab3500653d18842b53504ec06a10e2b375cc894
bywwh <1065449148@qq.com>
2016-10-06 16:39:58 +0800

    update custom_targetfiles.sh

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -1,7 +1,4 @@
 echo ">>> in $0"
 OUT_DIR=out/merged_target_files
-
-rm -v -rf $OUT_DIR/DATA
-mkdir -vp $OUT_DIR/DATA
 cp -v -rf other/supersu $OUT_DIR/META
 cp -v -rf other/file_contexts $OUT_DIR/META
