4957ed7d3a55ca9a69d3bf615544c73486a03870
bywwh <1065449148@qq.com>
2016-10-06 14:26:37 +0800

    remove v4a

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -3,6 +3,5 @@ OUT_DIR=out/merged_target_files
 
 rm -v -rf $OUT_DIR/DATA
 mkdir -vp $OUT_DIR/DATA
-cp -v -rf other/data $OUT_DIR/DATA
 cp -v -rf other/supersu $OUT_DIR/META
 cp -v -rf other/file_contexts $OUT_DIR/META
