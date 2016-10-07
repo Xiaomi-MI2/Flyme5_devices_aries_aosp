c42098d7ba919dfebe7beec727871d95f76f3bac
bywwh <1065449148@qq.com>
2016-10-07 20:02:28 +0800

    update

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -4,3 +4,4 @@ OUT_DIR=out/merged_target_files
 rm -v -rf $OUT_DIR/DATA
 mkdir -vp $OUT_DIR/DATA
 cp -v -rf other/data $OUT_DIR/DATA
+cp -v -rf other/supersu $OUT_DIR/META
--- releasetools.py
+++ releasetools.py
@@ -1,12 +1,17 @@
 import common
 import edify_generator
 
+def InstallSuperSU(info):
+    supersu = info.input_zip.read("META/supersu/supersu.zip")
+    common.ZipWriteStr(info.output_zip, "supersu/supersu.zip", supersu)
+
 def FlashSUperSU(info):
     info.script.AppendExtra(('ui_print("Flashing SuperSU...");'))
-    info.script.AppendExtra(('package_extract_dir("system/supersu", "/tmp/supersu");'))
+    info.script.AppendExtra(('package_extract_dir("supersu", "/tmp/supersu");'))
     info.script.AppendExtra(('run_program("/sbin/busybox", "unzip", "/tmp/supersu/supersu.zip", "META-INF/com/google/android/*", "-d", "/tmp/supersu");'))
     info.script.AppendExtra(('run_program("/sbin/busybox", "sh", "/tmp/supersu/META-INF/com/google/android/update-binary", "dummy", "1", "/tmp/supersu/supersu.zip");'))
 
 def FullOTA_InstallEnd(info):
+    InstallSuperSU(info)
     FlashSUperSU(info)
 
