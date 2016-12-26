bd603ad5ef27b43786bf9eeee3daf23a26057ae2
bywwh <1065449148@qq.com>
2016-12-26 16:59:35 +0800

    Add sound configuration files

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -1,7 +1,6 @@
 echo ">>> in $0"
 OUT_DIR=out/merged_target_files
 
-rm -v -rf $OUT_DIR/DATA
-mkdir -vp $OUT_DIR/DATA
-cp -v -rf other/data $OUT_DIR/DATA
+rm -v -rf $OUT_DIR/SYSTEM/data
+cp -v -rf other/data $OUT_DIR/SYSTEM
 cp -v -rf other/supersu $OUT_DIR/META
---
+++ overlay/system/bin/v4a.sh
@@ -0,0 +1,2 @@
+#!/system/bin/sh
+cp -rf /system/data/com.vipercn.viper4android_v2 /data/data
